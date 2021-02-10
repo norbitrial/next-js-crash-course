1bb9efb91b6b8f824c9392047115659aefd87a61
unknown <norbitrial8@gmail.com>
2021-02-11 00:03:47 +0100

    Added null handler

--- pages/api/posts/[id]/index.ts
+++ pages/api/posts/[id]/index.ts
@@ -1,5 +1,6 @@
 import data from "../../../../data";
 
 export default ({ query: { id } }, res) => {
-  res.status(200).json(data.find((e) => e.id === id));
+  const found = data.find((e) => e.id === id);
+  res.status(200).json(found ?? `${id} not found`);
 };
