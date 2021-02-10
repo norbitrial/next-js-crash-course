b6fdd5d095d7f15aeca89f4b15467850ae5679e5
unknown <norbitrial8@gmail.com>
2021-02-10 21:27:58 +0100

    Initial Next.js project commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Welcome!</title>
+        <meta name="keywords" content="web development, react" />
+      </Head>
+      <h1>Welcome Page</h1>
+    </div>
+  );
+}
